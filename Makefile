57ae8780282d1c8e84aed099cad1bc2b6111c9e2
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-12-01 15:36:50 -0600

    ok

---
+++ Makefile
@@ -0,0 +1,4 @@
+default: 
+	mpicc k-NN.c -o k-NN -lm
+	mpirun -np 2 ./k-NN
+
---
+++ k-NN.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>	// To use the sqrt function
+#include <string.h>	// To use the memcpy function
+// #include <time.h>	// To seed the randomizer
+
+#include <mpi.h>	// Main parallel library
+#include <unistd.h>	// Needed for gethostname and sleep
+
+
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+}
+
