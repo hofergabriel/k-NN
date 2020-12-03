242003bdb2faaab85f24f1c0991a839257f4ca29
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-12-02 18:00:29 -0600

    ok

--- Makefile
+++ Makefile
@@ -2,3 +2,8 @@ default:
 	mpicc k-NN.c -o k-NN -lm
 	mpirun -np 2 ./k-NN
 
+input:
+	mpicc k-NN.c -o k-NN -lm < in
+	mpirun -np 2 ./k-NN < in
+
+
--- k-NN.c
+++ k-NN.c
@@ -4,21 +4,41 @@
 #include <string.h>	// To use the memcpy function
 // #include <time.h>	// To seed the randomizer
 
-#include <mpi.h>	// Main parallel library
+//#include <mpi.h>	// Main parallel library
 #include <unistd.h>	// Needed for gethostname and sleep
 
+struct plant {
+  int sepal_l, sepal_w, petal_l, petal_w;
+	char class[20];
+};
 
+//void kNN(){ }
+/*
+float dist(plant a, plant b){
+	return (float)1.0;
+}*/
 
-
-
-int main(){
-
-
-
-
-
+int main(int argc, char ** argv){
+	char *buf = NULL;
+	size_t len;
+	float a, b, c, d;
+	char ch;
+	FILE *fp = fopen(argv[1],"r");
+	while(getline(&buf, &len, fp)>0){
+		getline(&buf, &len, fp);
+		sscanf(buf, "%f %[,] %f %[,] %f %[,] %f", &a, &ch, &b, &ch, &c, &ch, &d);
+		printf("%f %f %f %f\n", a, b, c, d);
+	}
 
 
 
 }
+
+
+
+
+
+
+
+
 
